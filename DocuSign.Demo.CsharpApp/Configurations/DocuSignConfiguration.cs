a500b1df5c39f11742894eb1505d778993604f67
Cyril Thirion <cyril.thirion+github@gmail.com>
2020-06-08 19:15:47 +0200

    cleaning DocuSignConfiguration

--- DocuSign.Demo.CsharpApp/Configurations/DocuSignConfiguration.cs
+++ DocuSign.Demo.CsharpApp/Configurations/DocuSignConfiguration.cs
@@ -2,19 +2,25 @@
 {
     public class DocuSignConfiguration
     {
+        /// <summary>
+        /// ClientID that will be create via the Developer
+        /// sandbox as described here: https://developers.docusign.com/id-tsp-api/guides
+        /// </summary>
         public string ClientId { get; set; }
+
+        /// <summary>
+        /// ClientSecret will be generated when creating the ClientId
+        /// </summary>
         public string ClientSecret { get; set; }
 
-        public string BaseUrl { get; set; }
-
-        #region IDP Configuration
+        /// <summary>
+        /// IDPServerBaseUrl depends which DocuSign environment you
+        /// are working with. Usually it will be
+        /// https://account-d.docusign.com Demo environment
+        /// https://account.docusign.com Prod environment
+        /// </summary>
         public string IDPServerBaseUrl { get; set; }
-        public string AuthorizationEndpoint { get; set; }
-        public string TokenEndpoint { get; set; }
-        public string UserInformationEndpoint { get; set; }
-        #endregion
 
-        public string AppUrl { get; set; }
 
         public DocuSignConfiguration()
         {
