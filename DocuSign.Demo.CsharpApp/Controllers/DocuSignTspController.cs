d6c015bde82eeda1c7784abd221d010e312252b1
Cyril Thirion <cyril.thirion+github@gmail.com>
2021-02-16 11:15:23 +0100

    Upgrade TspController to manage eSeal Flow

--- DocuSign.Demo.CsharpApp/Controllers/DocuSignTspController.cs
+++ DocuSign.Demo.CsharpApp/Controllers/DocuSignTspController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using DocuSign.Demo.CsharpApp.Services;
 using DocuSign.Demo.CsharpApp.Settings;
 using DocuSign.Demo.SignatureProvider.CA;
-using DocuSign.Tsp.Model;
+using DocuSign.Tsp.Model; 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -50,11 +50,10 @@ namespace DocuSign.Demo.CsharpApp.Controllers
             
 
             // Initialize DocuSign ApiClient
-            DocuSign.eSign.Client.Configuration dsConfiguration = new eSign.Client.Configuration();
-            dsConfiguration.ApiClient = new eSign.Client.ApiClient(signatureOAuthUserToken.user_api + "/restapi");
-            dsConfiguration.AddDefaultHeader("Authorization", "Bearer " + signatureOAuthUserToken.access_token);
+            var apiClient = new eSign.Client.ApiClient(signatureOAuthUserToken.user_api + "/restapi");
+            apiClient.Configuration.AddDefaultHeader("Authorization", "Bearer " + signatureOAuthUserToken.access_token);
 
-            DocuSign.eSign.Api.SignatureApi signatureApi = new eSign.Api.SignatureApi(dsConfiguration);
+            DocuSign.eSign.Api.SignatureApi signatureApi = new eSign.Api.SignatureApi(apiClient);
 
             // UserInfo
             var userInfoResult = signatureApi.UserInfo();
@@ -63,7 +62,7 @@ namespace DocuSign.Demo.CsharpApp.Controllers
 
             var SignHashSessionInfoResult = signatureApi.SignHashSessionInfo(new eSign.Model.SignSessionInfoRequest(Convert.ToBase64String(userCertificate.CertContainer.Certificate.GetEncoded())));
 
-            if (SignHashSessionInfoResult.Documents[0] == null || SignHashSessionInfoResult.Documents[0].RemainingSignatures == "0")
+            if (SignHashSessionInfoResult.Documents[0] == null || SignHashSessionInfoResult.Documents[0].RemainingSignatures == 0L)
             {
                 return Ok("Nothing to sign");
             }
@@ -83,5 +82,60 @@ namespace DocuSign.Demo.CsharpApp.Controllers
 
             return Redirect(CompleteSignHashResult.RedirectionUrl);
         }
+
+        #region eSeal Flow
+        [HttpGet("{env_slt}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> StarteSealTspFlow([FromQuery(Name = "env_slt")] string envSlt)
+        {
+            // First step is to get a token with the retrieve friom the query param
+            SignatureOAuthUserToken signatureOAuthUserToken;
+            try
+            {
+                signatureOAuthUserToken = await DocuSignService.GetBearerTokenFromEnvSltAsync(_httpClientFactory, _dsConfig, envSlt);
+            }
+            catch (Exception e)
+            {
+                // If you ending up in this case, make sure you clientId/clientSecret are good
+                // Also make sure you are hitting the right environement
+                return BadRequest(e);
+            }
+
+
+            // Initialize DocuSign ApiClient
+            var apiClient = new eSign.Client.ApiClient(signatureOAuthUserToken.user_api + "/restapi");
+            apiClient.Configuration.AddDefaultHeader("Authorization", "Bearer " + signatureOAuthUserToken.access_token);
+
+            DocuSign.eSign.Api.SignatureApi signatureApi = new eSign.Api.SignatureApi(apiClient);
+
+            // UserInfo
+            var userInfoResult = signatureApi.UserInfo();
+
+            UserCertificate userCertificate = new UserCertificate(userInfoResult.User.DisplayName, userInfoResult.User.Email, userInfoResult.Language.ToUpper(), "DocuSign");
+
+            var SignHashSessionInfoResult = signatureApi.SignHashSessionInfo(new eSign.Model.SignSessionInfoRequest(Convert.ToBase64String(userCertificate.CertContainer.Certificate.GetEncoded())));
+
+            if (SignHashSessionInfoResult.Documents[0] == null || SignHashSessionInfoResult.Documents[0].RemainingSignatures == 0L)
+            {
+                return Ok("Nothing to sign");
+            }
+
+            byte[] hashBytes = System.Convert.FromBase64String(SignHashSessionInfoResult.Documents[0].Data);
+            byte[] signedCmsData = SignatureProvider.SignatureProvider.Sign(hashBytes, userCertificate);
+
+            eSign.Model.DocumentUpdateInfo docInfo = new eSign.Model.DocumentUpdateInfo()
+            {
+                DocumentId = SignHashSessionInfoResult.Documents[0].DocumentId,
+                Data = Convert.ToBase64String(signedCmsData),
+                ReturnFormat = "CMS"
+            };
+            var CompleteSignHashResult = signatureApi.CompleteSignHash(new eSign.Model.CompleteSignRequest() { DocumentUpdateInfos = new List<eSign.Model.DocumentUpdateInfo> { docInfo } });
+
+            ViewBag.CompleteResult = CompleteSignHashResult;
+
+            return Redirect(CompleteSignHashResult.RedirectionUrl);
+        }
+        #endregion
     }
 }
--- DocuSign.Demo.CsharpApp/Services/DocuSignService.cs
+++ DocuSign.Demo.CsharpApp/Services/DocuSignService.cs
@@ -13,7 +13,8 @@ namespace DocuSign.Demo.CsharpApp.Services
 {
     public static class DocuSignService
     {
-        public static HttpRequestMessage BuildBearerTokenFromCodeRequest(DocuSignSettings dsconfig, string code)
+        #region eSign TSP Flow
+        private static HttpRequestMessage BuildBearerTokenFromCodeRequest(DocuSignSettings dsconfig, string code)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, dsconfig.IDPServerBaseUrl + "/oauth/token");
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -52,7 +53,50 @@ namespace DocuSign.Demo.CsharpApp.Services
 
             return JsonConvert.DeserializeObject<SignatureOAuthUserToken>(responseContent);
         }
+        #endregion
 
+        #region eSeal Flow
+        private static HttpRequestMessage BuildBearerTokenFromEnvSltRequest(DocuSignSettings dsconfig, string envSlt)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, dsconfig.IDPServerBaseUrl + "/oauth/token");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string authorizationHeaderValue = string.Format("{0}:{1}", dsconfig.ClientId
+                , dsconfig.ClientSecret);
+            string base64HeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes(authorizationHeaderValue));
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64HeaderValue);
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                // For eSeal should be. code from env_slt
+                new KeyValuePair<string, string>("grant_type", "docusign:params:oauth:grant-type:envelope-slt"),
+                new KeyValuePair<string, string>("env_slt", envSlt),
+                // The redirect_uri one is not really used in the TSP flow
+                new KeyValuePair<string, string>("redirect_uri", String.Format("https://{0}", "localhost:5001/api/docusigntsp/starttspflow")),
+            });
+
+            request.Content = content;
+
+            return request;
+        }
+
+        public static async Task<SignatureOAuthUserToken> GetBearerTokenFromEnvSltAsync(IHttpClientFactory httpClientFactory, DocuSignSettings dsConfig, string code)
+        {
+            HttpRequestMessage request = DocuSignService.BuildBearerTokenFromEnvSltRequest(dsConfig, code);
+
+            var client = httpClientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+
+            if (response.StatusCode < HttpStatusCode.OK && response.StatusCode >= HttpStatusCode.BadRequest)
+            {
+                throw new Exception(response.Headers.ToString() + "\n" + responseContent);
+            }
+
+            return JsonConvert.DeserializeObject<SignatureOAuthUserToken>(responseContent);
+        }
+        #endregion
     }
 
 }
