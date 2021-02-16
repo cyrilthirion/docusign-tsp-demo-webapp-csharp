ad1097c21233feddf64743afad0bc9ad1aac838e
Cyril Thirion <cyril.thirion+github@gmail.com>
2021-02-16 15:10:27 +0100

    Improve Unit Test

--- DocuSign.Demo.SignatureProviderTest/SignatureProviderTest.cs
+++ DocuSign.Demo.SignatureProviderTest/SignatureProviderTest.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Org.BouncyCastle.Asn1.Pkcs;
 using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Security;
 using Org.BouncyCastle.X509;
 using Org.BouncyCastle.X509.Store;
 
@@ -41,7 +45,7 @@ namespace DocuSign.Demo.SignatureProviderTest
 
                 Assert.AreEqual(cert.SubjectDN.ToString(), "CN=Cyril Thirion,E=cyril.thirion\\+github@gmail.com,C=FR,O=Acme");
 
-                Assert.IsNotNull(signer.SignedAttributes[PkcsObjectIdentifiers.Pkcs9AtContentType]);
+                Assert.AreEqual(signer.SignedAttributes[PkcsObjectIdentifiers.Pkcs9AtContentType].AttrValues[0].ToString(), PkcsObjectIdentifiers.Data.ToString());
                 Assert.IsNotNull(signer.SignedAttributes[PkcsObjectIdentifiers.Pkcs9AtMessageDigest]);
                 Assert.IsNotNull(signer.SignedAttributes[PkcsObjectIdentifiers.IdAASigningCertificateV2]);
                 Assert.IsNotNull(signer.SignedAttributes[PkcsObjectIdentifiers.Pkcs9AtSigningTime]);
@@ -50,6 +54,12 @@ namespace DocuSign.Demo.SignatureProviderTest
                     "#" + BitConverter.ToString(hashBytes).Replace("-", ""),
                     signer.SignedAttributes[PkcsObjectIdentifiers.Pkcs9AtMessageDigest].AttrValues[0].ToString(), true
                 );
+
+                byte[] signedData = signer.GetEncodedSignedAttributes();
+                ISigner s = SignerUtilities.GetSigner("SHA256withRSA");
+                s.Init(false, cert.GetPublicKey());
+                s.BlockUpdate(signedData, 0, signedData.Length);
+                Assert.IsTrue(s.VerifySignature(signer.GetSignature()), "Signature verification failed.");
             }
         }
     }
